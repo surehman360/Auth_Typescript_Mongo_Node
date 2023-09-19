8567d9b6348a9a10783a70c95316bd987684ce6d
unknown <salmanurrehman360@gmail.com>
2023-09-19 05:50:30 -0700

    Commit

---
+++ coverletter-backend/src/middlewares/index.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import {get, merge} from 'lodash';
+
+import { getUserBySessionToken } from '../db/users';
+
+
+export const isOwner =async (req:express.Request,res:express.Response,next:express.NextFunction) => {
+    try{
+        const {id}=req.params;
+        const currentUserid = get(req, 'identity.id') as string;
+
+        if (!currentUserid){
+            return res.sendStatus(403);
+        }
+        if (currentUserid.toString()!= id){
+            return res.sendStatus(403);
+        }
+        next();
+    }
+        catch(error)
+        {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try{
+        const sessionToken = req.cookies['Name-Auth'];
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+        const existingUser =await getUserBySessionToken(sessionToken);
+        if (!existingUser){
+            return res.sendStatus(403);
+        }
+
+        merge(req, {identity: existingUser});
+        return next();
+
+    }
+    catch (error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+} 
